0a0f84a476bba1c1431a1faef79f9e2992ae82db
Denis Buzdalov <public@buzden.ru>
2019-12-13 14:58:13 +0300

    Function definition was nicened.

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,6 +5,6 @@ fact 0 = 1
 fact n = n * fact (n - 1)
 
 nmlist :: Integer -> Integer -> [Integer]
-nmlist n m | n > m  = []
-           | n == m = [n]
-nmlist n m = n : nmlist (n + 1) m
+nmlist n m | n > m     = []
+           | n == m    = [n]
+           | otherwise = n : nmlist (n + 1) m

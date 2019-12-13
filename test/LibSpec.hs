b850608c2493084cd79c7b07fae3b9c4d0ca22ab
Denis Buzdalov <public@buzden.ru>
2019-12-13 15:46:22 +0300

    Testing facilities were added.

---
+++ test/LibSpec.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE BlockArguments #-}
+
+module LibSpec where
+
+import Test.Hspec
+
+spec :: Spec
+spec = describe "Lecture properties" do
+  describe "Factorial function" do
+    it "calculates right answer" pending
+    it "two variants are same" pending
+  describe "N-M-List function" do
+    it "calculates right answer" pending
+    it "two variants are same" pending

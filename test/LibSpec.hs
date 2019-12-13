5869704dd0d9caffa9e7cc08d9290105e5eafe90
Denis Buzdalov <public@buzden.ru>
2019-12-13 15:51:06 +0300

    Correct answer tests were added.

--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -2,13 +2,23 @@
 
 module LibSpec where
 
+import Lib
 import Test.Hspec
+import Test.QuickCheck
 
 spec :: Spec
 spec = describe "Lecture properties" do
+
   describe "Factorial function" do
-    it "calculates right answer" pending
+
+    it "calculates right answer" . property $ \(NonNegative n) ->
+      fact n === product [1 .. n]
+
     it "two variants are same" pending
+
   describe "N-M-List function" do
-    it "calculates right answer" pending
+
+    it "calculates right answer" . property $ \n m ->
+      nmlist n m === [n .. m]
+
     it "two variants are same" pending

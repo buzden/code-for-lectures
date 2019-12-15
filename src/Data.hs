d4a5b1b8c15d86fffc437e5dcdb8788fb1a5aad1
Denis Buzdalov <public@buzden.ru>
2019-12-15 15:33:39 +0300

    Some example functions were added.

--- src/Data.hs
+++ src/Data.hs
@@ -1,5 +1,7 @@
 module Data where
 
+import Data.List (intercalate)
+
 data List a = Nil | Cons a (List a)
 
 data BinTree a = BLeaf a | BNode (BinTree a) (BinTree a)
@@ -12,3 +14,17 @@ data JsonValue = JsonNull
                | JsonString String
                | JsonArray  [JsonValue]
                | JsonObject [(String, JsonValue)]
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' _ []     = []
+filter' f (x:xs) = if f x then x:tail else tail
+  where tail = filter f xs
+
+foldr' :: (a -> b -> b) -> b -> [a] -> b
+foldr' _ z []     = z
+foldr' f z (x:xs) = f x $ foldr f z xs
+
+jsToStr :: JsonValue -> String
+jsToStr JsonNull = "null"
+jsToStr (JsonArray vs) =
+  "[" ++ (intercalate ", " . map jsToStr $ vs) ++ "]"

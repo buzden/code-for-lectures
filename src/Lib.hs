869ad7e9375c85542e48b7bfeccabdf02dba460a
Denis Buzdalov <public@buzden.ru>
2019-12-24 09:03:31 +0300

    Kind signature with `Type` was added.

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,5 +3,10 @@
 
 module Lib where
 
+import Data.Kind (Type)
+
 prepend :: forall (a :: *). a -> [a] -> [a]
 prepend x xs = x:xs
+
+prepend' :: forall (a :: Type). a -> [a] -> [a]
+prepend' x xs = x:xs

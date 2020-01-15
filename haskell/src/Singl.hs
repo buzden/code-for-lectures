5f36d24f2e40063e85ea6d52a0697225c6706772
Denis Buzdalov <public@buzden.ru>
2020-01-15 20:30:39 +0300

    Partially defined instance of the singleton-based Eq for lists was added

--- haskell/src/Singl.hs
+++ haskell/src/Singl.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE DataKinds, KindSignatures, PolyKinds, TypeOperators, TypeFamilies #-}
+{-# LANGUAGE GADTs, UndecidableInstances #-}
 
 module Singl where
 
 import Data.Singletons (Demote, Sing, SingKind, fromSing, withSomeSing)
-import Data.Singletons.Decide ((:~:))
+import Data.Singletons.Decide ((:~:)(..))
+import Data.Singletons.Prelude.Bool
 import Prelude hiding (Eq, (==))
 
 class Eq a where
@@ -20,3 +22,35 @@ class Eq a where
 x == y = withSomeSing x $ \sX ->
            withSomeSing y $ \sY ->
              fromSing (sX %== sY)
+
+--- Instance ---
+
+data List a = Nil | Cons a (List a)
+
+data instance Sing (xs :: List a) where
+    SNil  :: Sing Nil
+    SCons :: Sing x -> Sing xs -> Sing (Cons x xs)
+
+instance Eq a => Eq (List a) where
+  type Nil       == Nil       = True
+  type Nil       == Cons _ _  = False
+  type Cons _ _  == Nil       = False
+  type Cons x xs == Cons y ys = x == y && xs == ys
+
+  SNil       %== SNil       = STrue
+  SNil       %== SCons _ _  = SFalse
+  SCons _ _  %== SNil       = SFalse
+  SCons x xs %== SCons y ys = x %== y %&& xs %== ys
+
+  eqRefl SNil         = Refl
+  eqRefl (SCons x xs) = case (eqRefl x, eqRefl xs) of
+    (Refl, Refl) -> Refl
+
+  eqSymm SNil SNil = Refl
+  eqSymm SNil (SCons _ _) = Refl
+  eqSymm (SCons _ _) SNil = Refl
+  eqSymm (SCons x xs) (SCons y ys) = case (eqSymm x y, eqSymm xs ys) of
+    (Refl, Refl) -> Refl
+
+  eqTran SNil SNil SNil Refl Refl = Refl
+  eqTran xs ys zs p q = undefined
